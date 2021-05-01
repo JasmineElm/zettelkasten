04cec910d0584c9ef82bc6bace194064f7506cd5
James Lemin <kj6fi170m6e@riseup.net>
2021-05-01 11:08:31 +0100

    sync: 2021-05-01 11:08

---
+++ github-sync.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+out_of_sync=$(git status --porcelain | wc -l)
+
+datestamp() {
+  date +"%Y-%m-%d %H:%M"
+} 
+
+pushit() {
+  git pull
+  git add .
+  git commit -q -m "sync: $(datestamp)"
+  git push
+
+}
+
+[ $out_of_sync -eq 0 ] || $(pushit)
+
